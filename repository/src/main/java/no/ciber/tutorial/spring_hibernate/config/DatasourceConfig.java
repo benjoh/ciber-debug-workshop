a8b5289fe79ba8516b9a4223562b259bd317cee4
Peter Breunig <peter.breunig@ciber.com>
2014-12-01 14:43:38 +0100

    Initsiell Hibernate config med HSQLDB

--- repository/src/main/java/no/ciber/tutorial/spring_hibernate/config/DatasourceConfig.java
+++ repository/src/main/java/no/ciber/tutorial/spring_hibernate/config/DatasourceConfig.java
@@ -1,4 +1,51 @@
 package no.ciber.tutorial.spring_hibernate.config;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
 public class DatasourceConfig {
+
+    private static final String MODEL_PACKAGE = "no.ciber.tutorial.spring_hibernate.model";
+    private static final String DB_DRIVER_CLASS = "org.hsqldb.jdbcDriver";
+    private static final String DB_CONNECTION_URL = "jdbc:hsqldb:mem:testdb";
+    private static final String DB_USERNAME = "sa";
+    private static final String DB_PASSWORD = "";
+
+    @Bean
+    public DataSource dataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName(DB_DRIVER_CLASS);
+        ds.setUrl(DB_CONNECTION_URL);
+        ds.setUsername(DB_USERNAME);
+        ds.setPassword(DB_PASSWORD);
+        return ds;
+
+    }
+
+    @Bean
+    @Autowired
+    public LocalSessionFactoryBean sessionFactoryBean(DataSource dataSource){
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setAnnotatedPackages(MODEL_PACKAGE);
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    private Properties hibernateProperties(){
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
+        hibernateProperties.setProperty("hibernate.dialect", "");
+        return hibernateProperties;
+    }
 }
--- web/src/main/java/no/ciber/tutorial/spring_hibernate/config/WebConfig.java
+++ web/src/main/java/no/ciber/tutorial/spring_hibernate/config/WebConfig.java
@@ -1,6 +1,8 @@
 package no.ciber.tutorial.spring_hibernate.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -13,5 +15,13 @@ public class WebConfig extends WebMvcConfigurerAdapter{
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         super.addResourceHandlers(registry);
         registry.addResourceHandler("resources/**").addResourceLocations("resources/");
+    }
+
+    @Controller
+    static class FaviconController {
+        @RequestMapping("favicon.ico")
+        String favicon() {
+            return "forward:/resources/images/favicon.ico";
+        }
     }
 }
