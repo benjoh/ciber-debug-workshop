b7d3fc9ea2bfac92adad7e27d472e6cd98100d8f
Peter Breunig <peter.breunig@ciber.com>
2014-12-02 23:09:05 +0100

    En liten fiks

--- services/src/main/java/no/ciber/tutorial/spring_hibernate/config/ServiceConfig.java
+++ services/src/main/java/no/ciber/tutorial/spring_hibernate/config/ServiceConfig.java
@@ -1,5 +1,8 @@
 package no.ciber.tutorial.spring_hibernate.config;
 
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "no.ciber.tutorial.spring_hibernate.services")
 public class ServiceConfig {
 
 }
